715af466f135dcbe42182d580045ac07a3fe540a
Fox <e.kizhin@yandex.ru>
2024-10-16 18:26:16 +0300

    Update main.cpp
    - Added validation of rows/columns input
    - Added error hadling for a call of createMatrix
    - Added input and output of matrix

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
-#include <iostream>
-
 #include "matrix.h"
 
 int main() {
-    size_t rows = 0;
-    size_t columns = 0;
+  size_t rows = 0;
+  size_t columns = 0;
 
-    std::cin >> rows >> columns;
-    int** matrix = createMatrix(rows, columns);
+  if (!(std::cin >> rows >> columns)) {
+    return 1;
+  }
+  int** matrix = nullptr;
+  try {
+    matrix = createMatrix(rows, columns);
+  } catch (std::bad_alloc& e) {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
+  };
 
-    deleteMatrix(matrix, rows, columns);
+  inputMatrix(std::cin, matrix, rows, columns);
+  if (!std::cin.good()) {
+    deleteMatrix(matrix, rows);
+    return 1;
+  }
+  outputMatrix(std::cout, matrix, rows, columns);
+
+  deleteMatrix(matrix, rows);
 }
