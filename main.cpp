cb48f675d7b5257852ed049210589718617e308c
Fox <e.kizhin@yandex.ru>
2024-10-16 11:33:44 +0300

    Update main.cpp
    - Added prototypes and calls of functions createMatrix and
      deleteMatrix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+int** createMatrix(size_t, size_t);
+void deleteMatrix(int**, size_t, size_t);
+
 int main() {
-    size_t M = 0;
-    size_t N = 0;
-    std::cin >> M >> N;
+    size_t rows = 0;
+    size_t columns = 0;
+
+    std::cin >> rows >> columns;
+    int** matrix = createMatrix(rows, columns);
+
+    deleteMatrix(matrix, rows, columns);
 }
