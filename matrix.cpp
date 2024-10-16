848a8aecc6c663b6de5944143437e3820c81f9fb
Fox <e.kizhin@yandex.ru>
2024-10-16 18:24:09 +0300

    Update matrix.cpp
    Implemented all functions declared in matrix.h:
    - create/deleteMatrix
    - input/outputMatrix

--- matrix.cpp
+++ matrix.cpp
@@ -1,22 +1,44 @@
 #include "matrix.h"
 
-int** createMatrix(int**, size_t rows, size_t columns) {
-    int** matrix = new int*[rows];
-    size_t created = 0;
-    try {
-        for (; created < columns; ++created) {
-            matrix[created] = new int[columns];
-        }
-    } catch (const std::bad_alloc& e) {
-        deleteMatrix(matrix, created);
-        throw;
+int** createMatrix(const size_t rows, const size_t columns) {
+  int** matrix = new int*[rows];
+  size_t createdRows = 0;
+  try {
+    for (; createdRows < rows; ++createdRows) {
+      matrix[createdRows] = new int[columns];
     }
-    return matrix;
+  } catch (const std::bad_alloc& e) {
+    deleteMatrix(matrix, createdRows);
+    throw;
+  }
+  return matrix;
 }
 
-void deleteMatrix(int** matrix, size_t rows) {
-    for (size_t i = 0; i < rows; ++i) {
-        delete[] matrix[i];
-    }
-    delete[] matrix;
+void deleteMatrix(int* const* matrix, const size_t rows) {
+  for (size_t i = 0; i < rows; ++i) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
+}
+
+void inputMatrix(std::istream& is,
+                 int* const* matrix,
+                 const size_t rows,
+                 const size_t columns) {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < columns; ++j)
+      is >> matrix[i][j];
+  }
+}
+
+void outputMatrix(std::ostream& os,
+                  const int* const* matrix,
+                  const size_t rows,
+                  const size_t columns) {
+  for (size_t i = 0; i < rows; ++i) {
+    os << matrix[i][0];
+    for (size_t j = 1; j < columns; ++j)
+      os << ' ' << matrix[i][j];
+    os << '\n';
+  }
 }
