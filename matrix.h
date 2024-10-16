4271dd6745aa700b9724451ab42c4629a3deeefc
Fox <e.kizhin@yandex.ru>
2024-10-16 18:20:51 +0300

    Update matrix.h
    - Added const in deleteMatrix and inputMatrix prototypes

--- matrix.h
+++ matrix.h
@@ -4,9 +4,9 @@
 #include <iostream>
 
 int** createMatrix(const size_t, const size_t);
-void deleteMatrix(int**, const size_t);
+void deleteMatrix(int* const*, const size_t);
 
-int** inputMatrix(std::istream&, const size_t, const size_t);
+void inputMatrix(std::istream&, int* const*, const size_t, const size_t);
 void outputMatrix(std::ostream&, const int* const*, const size_t, const size_t);
 
 #endif
