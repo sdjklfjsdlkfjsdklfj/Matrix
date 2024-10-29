ca58896e5673597c936ddb02bf2e2f13f13ad925
fox <e.kizhin@yandex.ru>
2024-10-29 17:56:33 +0300

    Update main.cpp
    Used Matrix class instead of functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "matrix.h"
+#include <exception>
 
 int main()
 {
@@ -6,22 +7,19 @@ int main()
   size_t columns = 0;
 
   if (!(std::cin >> rows >> columns)) {
+    std::cerr << "Invalid input\n";
     return 1;
   }
-  int** matrix = nullptr;
   try {
-    matrix = createMatrix(rows, columns);
-  } catch (std::bad_alloc& e) {
+    Matrix matrix(rows, columns);
+    matrix.input(std::cin);
+    if (!std::cin.good()) {
+      std::cerr << "Invalid input\n";
+      return 1;
+    }
+    matrix.output(std::cout);
+  } catch (std::exception& e) {
     std::cerr << "Error: " << e.what() << '\n';
     return 1;
   };
-
-  inputMatrix(std::cin, matrix, rows, columns);
-  if (!std::cin.good()) {
-    deleteMatrix(matrix, rows);
-    return 1;
-  }
-  outputMatrix(std::cout, matrix, rows, columns);
-
-  deleteMatrix(matrix, rows);
 }
